ace02a10b011cfa0a38c25c573dbc4f73eba0c0e
David <davidhackerdvm@gmail.com>
2011-11-30 00:14:02 -0600

    add librds_util.so to prebuilts

--- targa-vendor-blobs.mk
+++ targa-vendor-blobs.mk
@@ -34,6 +34,7 @@ vendor/motorola/targa/proprietary/Hostapd:/system/bin/Hostapd \
 vendor/motorola/targa/proprietary/libreference-ril.so:/system/lib/libreference-ril.so \
 vendor/motorola/targa/proprietary/libril.so:/system/lib/libril.so \
 vendor/motorola/targa/proprietary/libril_rds.so:/system/lib/libril_rds.so \
+vendor/motorola/targa/proprietary/librds_util.so:/system/lib/librds_util.so \
 
 #vendor/motorola/targa/proprietary/batch:/system/bin/batch \
 #vendor/motorola/targa/proprietary/btcmd:/system/bin/btcmd \
@@ -245,7 +246,6 @@ vendor/motorola/targa/proprietary/hostapd.conf.templet:/system/etc/wifi/hostapd.
 #vendor/motorola/targa/proprietary/libprojectM.so:/system/lib/libprojectM.so \
 #vendor/motorola/targa/proprietary/libprovlib.so:/system/lib/libprovlib.so \
 #vendor/motorola/targa/proprietary/libquicksec.so:/system/lib/libquicksec.so \
-#vendor/motorola/targa/proprietary/librds_util.so:/system/lib/librds_util.so \
 #vendor/motorola/targa/proprietary/libscalado.so:/system/lib/libscalado.so \
 #vendor/motorola/targa/proprietary/libsmapi.so:/system/lib/libsmapi.so \
 #vendor/motorola/targa/proprietary/libspellingcheckengine.so:/system/lib/libspellingcheckengine.so \
