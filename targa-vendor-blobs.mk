4fbe7bd9bfa59b4c6babf520a2f90feb90026400
dhacker29 <davidhackerdvm@gmail.com>
2011-12-23 00:25:12 -0600

    revert to prebuilt whisperd

--- targa-vendor-blobs.mk
+++ targa-vendor-blobs.mk
@@ -282,8 +282,8 @@ vendor/motorola/targa/proprietary/libmotintfutil.so:/system/lib/libmotintfutil.s
 vendor/motorola/targa/proprietary/libportaljni.so:/system/lib/libportaljni.so \
 vendor/motorola/targa/proprietary/key_code_map.txt:system/etc/motorola/12m/key_code_map.txt \
 vendor/motorola/targa/proprietary/suplcerts.bks:system/etc/security/suplcerts.bks \
+vendor/motorola/targa/proprietary/whisperd:/system/bin/whisperd \
 
-#vendor/motorola/targa/proprietary/whisperd:/system/bin/whisperd \
 #vendor/motorola/targa/proprietary/libmoto_cdma_ril.so:/system/lib/libmoto_cdma_ril.so \
 #vendor/motorola/targa/proprietary/libmoto_db_ril.so:/system/lib/libmoto_db_ril.so \
 #vendor/motorola/targa/proprietary/libmoto_intfutil_ril:/system/lib/libmoto_intfutil_ril.so \
